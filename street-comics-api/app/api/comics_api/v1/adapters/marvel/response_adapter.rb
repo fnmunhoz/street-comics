1df66576d39d5d3fd8ef3423506019d056e0ece2
Felipe Munhoz <felipe@felipemunhoz.com>
2021-07-31 21:04:42 -0300

    feat(API): add get /api/v1/comics endpoint
    WIP, but already retrieves data with success from the marvel API
    
    Plays with some ideas to decouple our own API from external APIs

---
+++ street-comics-api/app/api/comics_api/v1/adapters/marvel/response_adapter.rb
@@ -0,0 +1,16 @@
+module ComicsApi
+  module V1
+    module Adapters
+      module Marvel
+        class ResponseAdapter
+          
+          def initialize(args = nil) end
+
+          def comics(response)
+            response
+          end
+        end
+      end
+    end
+  end
+end
