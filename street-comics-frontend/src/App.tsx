7a935507b5d31100f3950555aca245b78cb8b331
Felipe Munhoz <felipe@felipemunhoz.com>
2021-08-08 16:09:08 -0300

    chore(config): change react-query defaults
    The app data doesn't change often, so there is no benefit on refetch on windowFocus or reconnect

--- street-comics-frontend/src/App.tsx
+++ street-comics-frontend/src/App.tsx
@@ -3,7 +3,15 @@ import { QueryClient, QueryClientProvider } from 'react-query';
 import ComicsPage from './pages/comics/ComicsPage';
 
 const App = () => {
-  const queryClient = new QueryClient();
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        retry: false,
+        refetchOnWindowFocus: false,
+        refetchOnReconnect: false,
+      },
+    },
+  });
 
   return (
     <QueryClientProvider client={queryClient}>
